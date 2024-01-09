d84fa898c73e28c545c4de0915e13b61ea412ee6
moon-2000 <amanywaheeb2000@gmail.com>
2024-01-09 22:12:44 +0100

    drawing line in passenger videos

--- draw_line.py
+++ draw_line.py
@@ -1,59 +1,80 @@
 import cv2
+import os
 
-video_path = '/home/amany/Desktop/video_draw_entry/2023_10_09/008/19/train_008_cam_19_dt_2023_10_09_tc_02_53_04_Puttgarden.mp4'
-cap = cv2.VideoCapture(video_path)
 
-# Counter to keep track of processed frames
-frame_count = 0
-max_frames = 400
-start_frame = 300
+# Path to the directory containing the videos
+video_directory = '/home/amany/Desktop/video_draw_entry/2023_10_09/008/19'
 
-# Set the starting frame
-cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+# Path to the directory to save processed videos
+output_directory = '/home/amany/Desktop/video_draw_entry/2023_10_09/008/19_processed'
 
-# To save the captured video
-fps = 20  # frame per second
 
-# To get the width and height of the captured frame
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fourcc = cv2.VideoWriter_fourcc(*'MJPG')  # Change the codec to MJPEG
-output_path = 'processed_video.avi'
-writer = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+#video_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(video_directory) for f in filenames if f.endswith('.mp4')]
+video_files = [f for dp, dn, filenames in os.walk(video_directory) for f in filenames if f.endswith('.mp4')]
+print(f'number of videos to be processed is {len(video_files)}')
+print(f'video files {video_files}')
 
-# Define points for start/entry line and cross line
-entry_line_start = (268, 238)
-entry_line_end = (246, 323)
-cross_line_start = (340, 250)
-cross_line_end = (320, 350)
 
-if not cap.isOpened():
-    print('Error! File NOT Found.')
+for video_file in video_files:
+    # Construct the full path of the video file
+    input_video_path = os.path.join(video_directory, video_file)
+    cap = cv2.VideoCapture(input_video_path)
 
-while cap.isOpened() and frame_count < max_frames:
-    ret, frame = cap.read()
-    if not ret:
-        break
+    # To get the width and height of the captured frame
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')  # Change the codec to MJPEG
+
+    # To save the captured video
+    fps = 20  # frame per second
+
     
-    # Draw start/entry point line
-    cv2.line(frame, entry_line_start, entry_line_end, (0, 0, 255), 2)
-    
-    # Draw cross line
-    cv2.line(frame, cross_line_start, cross_line_end, (0, 0, 255), 2)
+    # Construct the output video path
+    output_name = video_file.replace('.mp4', '_processed.avi')
+    output_path = os.path.join(output_directory, output_name)
+    print(f'\n\n output path {output_path}')
+    writer = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
 
-    # Display the frame with the lines
-    cv2.imshow('Frame with Lines', frame)
 
-    # Increment the frame count
-    frame_count += 1
+    # Define points for start/entry line and cross line
+    entry_line_start = (268, 238)
+    entry_line_end = (246, 323)
+    cross_line_start = (340, 250)
+    cross_line_end = (326, 326)
 
-    # Save the frame into processed_video
-    writer.write(frame)
+    if not cap.isOpened():
+        print('Error! File NOT Found.')
 
-    # Press 'q' to exit the video
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        # Draw start/entry point line
+        cv2.line(frame, entry_line_start, entry_line_end, (0, 0, 255), 2)
+        
+        # Draw cross line
+        cv2.line(frame, cross_line_start, cross_line_end, (0, 0, 255), 2)
 
-cap.release()
-writer.release()  # To stop saving frames
-cv2.destroyAllWindows()
+        # Display the frame with the lines
+        cv2.imshow('Frame with Lines', frame)
+
+
+        # Save the frame into processed_video
+        writer.write(frame)
+
+        # Press 'q' to exit the video
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    writer.release()  # To stop saving frames
+    cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
